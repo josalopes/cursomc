01c07cf64bedd6c1d121fcdb96731e3093f61aa5
Francisco Josafá <josafa.lopes@gmail.com>
2021-08-04 12:11:46 -0300

    Recurso PRODUTO e associação ManyToMany com o recurso CATEGORIA

--- src/main/java/com/josafa/cursomc/domain/Categoria.java
+++ src/main/java/com/josafa/cursomc/domain/Categoria.java
@@ -1,24 +1,28 @@
 package com.josafa.cursomc.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 
 @Entity
 public class Categoria implements Serializable {
-	/**
-	 * 
-	 */
+	
 	private static final long serialVersionUID = 1L;
 	
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
+	
+	@ManyToMany(mappedBy="categorias")
+	private List<Produto> produtos = new ArrayList<>();
 	
 	public Categoria() {}
 
@@ -43,6 +47,14 @@ public class Categoria implements Serializable {
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+	
+	public List<Produto> getProdutos() {
+		return produtos;
+	}
+
+	public void setProdutos(List<Produto> produtos) {
+		this.produtos = produtos;
+	}
 
 	@Override
 	public int hashCode() {
@@ -60,6 +72,8 @@ public class Categoria implements Serializable {
 		Categoria other = (Categoria) obj;
 		return Objects.equals(id, other.id);
 	}
+
+	
 	
 	
 }
